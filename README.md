a96d653251d4efafffb06e8d6c9b88bd884fed7e
Bayu Wibowo <bayupw@gmail.com>
2022-10-25 08:43:58 +1300

    update readme.md, metal-gateway.tf, outputs.tf

--- README.md
+++ README.md
@@ -37,11 +37,34 @@ module "metal-vmware-esxi" {
   create_user_ssh_key  = false
 }
 
-output "metal-vmware-esxi" {
-  value     = module.metal-vmware-esxi
-  sensitive = true
+output "metal_device_ip" {
+  description = "VMware ESXi Public IP."
+  value       = module.metal-vmware-esxi.equinix_metal_device.access_public_ipv4
+}
+
+output "metal_device_password" {
+  description = "VMware ESXi password."
+  value       = module.metal-vmware-esxi.equinix_metal_device.root_password
+  sensitive   = true
+}
+
+output "metal_gateway_ip" {
+  description = "Metal Gateway IP."
+  value       = cidrhost(module.metal-vmware-esxi.equinix_metal_reserved_ip_block[0].cidr_notation, 1)
+}
+
+output "public_ip_block" {
+  description = "Public IP CIDR block"
+  value       = module.metal-vmware-esxi.equinix_metal_reserved_ip_block[0].cidr_notation
+}
+
+output "usable_public_ip" {
+  description = "Usable public IP range"
+  value       = "${cidrhost(module.metal-vmware-esxi.equinix_metal_reserved_ip_block[0].cidr_notation, 2)} - ${cidrhost(module.metal-vmware-esxi.equinix_metal_reserved_ip_block[0].cidr_notation, module.metal-vmware-esxi.equinix_metal_reserved_ip_block[0].quantity - 2)}"
 }
 ```
+
+In order to provide internet access to the VMs, the module by default creates an Equinix Metal Gateway. To disable Equinix Metal Gateway creation, set create_metal_gateway to true.
 
 To create a user ssh key, set ```create_user_ssh_key``` to true and provide the details in variable ```user_ssh_key_name``` and ```user_public_key```
 
--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,19 @@
-output "metal_device_ip" {
-  description = "VMware ESXi Public IP."
-  value       = equinix_metal_device.this.access_public_ipv4
+output "equinix_metal_device" {
+  description = "Equinix metal device object."
+  value       = equinix_metal_device.this
 }
 
-output "metal_device_password" {
-  description = "VMware ESXi password."
-  value       = equinix_metal_device.this.root_password
-  sensitive   = true
+output "equinix_metal_vlan" {
+  description = "Equinix metal vlan object."
+  value       = var.create_metal_gateway ? equinix_metal_vlan.this : null
 }
 
-output "metal_gateway_ip" {
-  description = "Metal Gateway IP."
-  value       = var.create_metal_gateway ? cidrhost(equinix_metal_reserved_ip_block.this[0].cidr_notation, 1) : null
+output "equinix_metal_gateway" {
+  description = "Equinix metal gateway object."
+  value       = var.create_metal_gateway ? equinix_metal_gateway.this : null
 }
 
-output "public_ip_block" {
-  description = "Public IP CIDR block"
-  value       = var.create_metal_gateway ? equinix_metal_reserved_ip_block.this[0].cidr_notation : null
-}
-
-output "usable_public_ip" {
-  description = "Usable public IP range"
-  value       = var.create_metal_gateway ? "${cidrhost(equinix_metal_reserved_ip_block.this[0].cidr_notation, 2)} - ${cidrhost(equinix_metal_reserved_ip_block.this[0].cidr_notation, var.num_public_ip - 2)}" : null
+output "equinix_metal_reserved_ip_block" {
+  description = "Equinix metal reserved IP block object."
+  value       = var.create_metal_gateway ? equinix_metal_reserved_ip_block.this : null
 }
